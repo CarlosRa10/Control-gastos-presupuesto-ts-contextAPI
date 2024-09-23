b727770b05d9893b9fa556900f2abaf7088ac795
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-09-23 16:44:31 -0400

    commit

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -5,13 +5,15 @@ import ExpenseDetail from "./ExpenseDetail"
 export default function ExpenseList() {
     const {state} = useBudget()
 
-    const isEmpty = useMemo(()=> state.expenses.length===0,[ state.expenses])
+    //Si state.currentCategory tiene un valor-Se aplica un filtro a la lista de gastos-El filtro selecciona solo aquellos gastos cuyo valor de la propiedad category coincide con el valor de state.currentCategory.
+    const filteredExpenses = state.currentCategory ? state.expenses.filter(expense=>expense.category===state.currentCategory):state.expenses//categoría seleccionada.
+    const isEmpty = useMemo(()=> filteredExpenses.length===0,[ filteredExpenses])
   return (
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay Gastos</p>:(
             <>
                 <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos.</p>
-                {state.expenses.map(expense=> (
+                {filteredExpenses.map(expense=> (
                     <ExpenseDetail
                         key={expense.id}
                         expense={expense}
